5a62baf8939d56deaad5a08c6a779f6c4908f62e
Tiago Costa Rebelo <43050965+tiagocostarebelo@users.noreply.github.com>
2025-08-07 15:11:49 +0000

    updated project to use localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { PiNotepad } from "react-icons/pi";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import NoteForm from "./components/NoteForm";
 import NoteList from "./components/NoteList";
 
 const App = () => {
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(() => {
+    const notes = JSON.parse(localStorage.getItem('notes'));
+    return notes || [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }, [notes]);
 
   const deleteNote = (id) => {
     const confirmDelete = window.confirm('Are you sure you want to delete this note?');
